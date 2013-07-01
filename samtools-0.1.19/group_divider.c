1a51a5f879aafbc715348be9aa9dcda9a32ba8e4
Diver <diver@spbau.ru>
2013-07-01 05:00:13 +0400

    Continuing creating bed_array

--- samtools-0.1.19/group_divider.c
+++ samtools-0.1.19/group_divider.c
@@ -13,17 +13,17 @@
 #define SCAFFOLD_MARKER '>'
 #define STD_ERROR 0.1
 
-/*
+
 void flushbed (FILE* fout, char* chr, int start, int end) {
 	if (fout == NULL) {
-		printf("Bed file is closed\n");
+		fprintf (stderr,"Bed file is closed\n");
 	}
 	fprintf (fout, "%s\t%d\t%d\n",chr,start,end);
 }
-*/
+
 
 void echobed (char* chr, int start, int end) {
-	printf ("[BED] %s\t%d\t%d\n",chr,start,end);
+	fprintf (stderr,"[BED] %s\t%d\t%d\n",chr,start,end);
 }
 
 size_t get_length (FILE* finput) {
@@ -36,21 +36,67 @@ size_t get_length (FILE* finput) {
 		length += (strlen(inbuf) - 1);
 	}
 	fseek(finput, 0, SEEK_SET);
-	//printf("%u\n", length);
+	//fprintf (stderr,"%u\n", length);
 	return length;
 }
 
-/*
+//Generating array of BEDS to return;
+typedef struct bed_list bed_list;
+struct bed_list {
+	char* bed;
+	bed_list* next;
+};
+bed_list* beds_array = NULL;
+bed_list* bed_tail;
+void beds_array_add (char* newbed) {
+	bed_list* bed = malloc (sizeof(beds_array));
+	if (beds_array==NULL) {
+		beds_array = bed;
+		bed_tail = beds_array;
+	}
+	else {
+		bed_tail->next = bed;
+		bed_tail = bed_tail->next;
+	}
+	fprintf (stderr,"Adding BED %s...\n", newbed);
+	bed_tail->bed = newbed;
+}
+
+int bed_list_count () {
+	bed_list* bed = beds_array;
+	int i=0;
+	if (bed==NULL) {fprintf (stderr,"BUG!\n"); return 0;}
+	while (bed) {
+		i++;
+		bed = bed->next;
+	}
+	return i;
+}
+
+char** bed_array_return () {
+	char** result = malloc (sizeof(char*) * 10/*bed_list_count()*/);
+	char** cur = result;
+	bed_list* cbed = beds_array;
+	if (cbed==NULL) fprintf (stderr,"BUG!\n");
+	while (cbed) {
+		*cur++ = cbed->bed;
+		fprintf (stderr,"BED is %s!\n", cbed->bed);
+		cbed=cbed->next;
+	}
+	return result;
+}
+
 FILE * open_bed(FILE* fbed, char * refname, int currfile, char * outname) {
 	if (fbed != NULL) {
 		fclose(fbed);
 	}
-	sprintf(outname, "%s.%i.bed", refname, currfile);
+	sprintf (outname, "%s.%i.bed", refname, currfile);
+	beds_array_add (outname);
 	return fopen(outname, "w");
 }
-*/
+
 void start_bed (int threadnum) {
-	printf ("Opening thread #%d\n",threadnum);
+	fprintf (stderr,"Opening thread #%d\n",threadnum);
 }
 
 void get_chrname(char * chrname, char inbuf[]) {
@@ -60,8 +106,8 @@ void get_chrname(char * chrname, char inbuf[]) {
 	chrname[size] = '\0';
 }
 
-int group_divider (char* filename, int threads) {
-	printf ("Group Divider!\n");
+int group_divider (char* filename, int threads, char** beds) {
+	fprintf (stderr,"Group Divider!\n");
 	FILE* finput = NULL;
 	FILE* fbed = NULL;
 	char inbuf[STRSIZE];
@@ -78,30 +124,32 @@ int group_divider (char* filename, int threads) {
 	char* refname;
 
 //	if (argc != 3) {
-//		printf("USAGE: ./group_divider reference.fasta files_number\n");
+//		fprintf (stderr,"USAGE: ./group_divider reference.fasta files_number\n");
 //		return 0;
 //	}
 
-	printf ("Opening in file %s\n", filename);
+	fprintf (stderr,"Opening in file %s\n", filename);
 	finput = fopen(filename,"r");
 	if (finput == NULL)  {
-		printf ("file open error #%d !\n", errno);
+		fprintf (stderr,"file open error #%d !\n", errno);
 		return 1;
 	}
 
-/*
+
 	curr = basename(filename);
 	refname = malloc (strchr(curr, '.') - curr);
 	strncpy(refname, curr, strchr(curr, '.') - curr);
-	printf("%s\n", refname);
+	fprintf (stderr,"%s\n", refname);
 
 	outname = malloc (strlen(filename)+8);
+//	fprintf (stderr,"Outname = %s\n",outname);
 	fbed = open_bed(fbed, refname, currfile, outname);
-*/
-	start_bed (currfile);
+
+//	start_bed (currfile);
+
 
 //	if (argc == 3) {
-		printf("Counting blocks size for %d threads\n", threads);
+		fprintf (stderr,"Counting blocks size for %d threads\n", threads);
 		files_num = threads;
 //	}
 	ref_length = get_length(finput);
@@ -116,12 +164,12 @@ int group_divider (char* filename, int threads) {
 
 		if (*inbuf == SCAFFOLD_MARKER) {
 			if (currpos) {
-//				flushbed (fbed, chrname,regstart,currpos);
-				echobed (chrname,regstart,currpos);
+				flushbed (fbed, chrname,regstart,currpos);
+//				echobed (chrname,regstart,currpos);
 				lastchr = globalpos;
 			}
 			get_chrname(chrname, inbuf);
-			printf ("Parsing scaffold '%s'\n", chrname);
+			fprintf (stderr,"Parsing scaffold '%s'\n", chrname);
 			regstart = ncount = currpos = 0;
 			continue;
 		}
@@ -130,27 +178,27 @@ int group_divider (char* filename, int threads) {
 
 		if (globalpos >= block_size * (currfile + STD_ERROR)) {
 			currfile += 1;
-//			printf("Bed file #%i is started\n", currfile);
+			fprintf (stderr,"Bed file #%i is started\n", currfile);
 			if (lastchr > block_size * (currfile - 1 - STD_ERROR)) {
-//				fbed = open_bed(fbed, refname, currfile, outname);
-				start_bed (currfile);
+				fbed = open_bed(fbed, refname, currfile, outname);
+//				start_bed (currfile);
 			} else if (last_n_end > block_size * (currfile - 1 - STD_ERROR)) {
-//				flushbed(fbed, chrname, regstart, last_n_end);
-				echobed (chrname,regstart,last_n_end);
+				flushbed(fbed, chrname, regstart, last_n_end);
+//				echobed (chrname,regstart,last_n_end);
 				regstart = last_n_end;
 			} else {
-//				flushbed(fbed, chrname, regstart, currpos - block_size * STD_ERROR);
-				echobed (chrname,regstart,currpos - block_size * STD_ERROR);
+				flushbed(fbed, chrname, regstart, currpos - block_size * STD_ERROR);
+//				echobed (chrname,regstart,currpos - block_size * STD_ERROR);
 				regstart = currpos - block_size * STD_ERROR;
-//				fbed = open_bed(fbed, refname, currfile, outname);
-				start_bed (currfile);
+				fbed = open_bed(fbed, refname, currfile, outname);
+//				start_bed (currfile);
 			}
 		}
 
 		curr = inbuf;
 		while (*curr) {
 			if (*curr == '\n') {
-				printf ("Malformed data!\n");
+				fprintf (stderr,"Malformed data!\n");
 			}
 			if (*curr == N ) {
 				ncount++;
@@ -169,9 +217,10 @@ int group_divider (char* filename, int threads) {
 		}
 	}
 	if (currpos) {
-//		flushbed (fbed, chrname,regstart,currpos);
-		echobed (chrname,regstart,currpos);
+		flushbed (fbed, chrname,regstart,currpos);
+//		echobed (chrname,regstart,currpos);
     }
+	beds =  bed_array_return ();
 	return 0;
 }
 	
